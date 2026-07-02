8466fd8fb908430b6efe0adf1785f4b85fe5472e
sama saif <samaalramadhani@gmail.com>
2026-07-03 00:50:48 +0400

    Task 9

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -160,7 +160,48 @@ internal class Program
         Console.WriteLine("final amount: " + amount);
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////
         
+        Console.WriteLine("\n");
+        // Task 9 - Day Name Finder
         
+        Console.WriteLine("enter a number from 1 to 7 representing a day of the week: ");
+        int day = int.Parse(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     }
     
 }
