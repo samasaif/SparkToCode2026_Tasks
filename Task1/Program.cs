f321db19dd5e520fe7e062eeea199c94815a0120
sama saif <samaalramadhani@gmail.com>
2026-07-03 00:43:19 +0400

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Task 1 - Personal Info Card
+        string name = "Sama";
+        int age = 22;
+        double heightInMeters = 1.61;
+        bool isStudent = true;
+        
+        Console.Write("Name: " + name + ", Age: " + age + ", Height: " + heightInMeters + ", Student: " + isStudent);
+        Console.Write("\n");
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
