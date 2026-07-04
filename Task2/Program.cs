8a37eb2e2322f44bc85d52326b57f57e484c5380
sama saif <samaalramadhani@gmail.com>
2026-07-04 17:05:02 +0400

    Task 1 from part 2 is implemented

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Task 1 - Countdown Timer
+       Console.WriteLine("please enter a starting number: ");
+       int number = Convert.ToInt32(Console.ReadLine());
+
+       for (int i = number; i >= 1 ; i--)
+       {
+        Console.WriteLine(i);   
+       }
+       Console.WriteLine("Liftoff!");  
+       Console.WriteLine("\n");
+       ////////////////////////////////////////////////////////////////////////////////////////////////
+       
+    }
+    
+}
