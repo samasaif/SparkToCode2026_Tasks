34b4b9b874353ccb7175ec48d590fb971733469e
sama saif <samaalramadhani@gmail.com>
2026-07-04 17:19:20 +0400

    task 2 - part 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,6 +5,9 @@ class Program
     static void Main(string[] args)
     {
        // Task 1 - Countdown Timer
+       /*
+         
+        
        Console.WriteLine("please enter a starting number: ");
        int number = Convert.ToInt32(Console.ReadLine());
 
@@ -14,7 +17,24 @@ class Program
        }
        Console.WriteLine("Liftoff!");  
        Console.WriteLine("\n");
+       */
        ////////////////////////////////////////////////////////////////////////////////////////////////
+       
+       // Task 2 - Sum of Numbers 1 to N
+       /*
+       Console.Write("please enter a positive whole number: ");
+       int positiveNumber = Convert.ToInt32(Console.ReadLine());
+
+       int total = 0;
+       for (int i = 0; i <= positiveNumber; i++)
+       {
+           total += i;
+       }
+       Console.WriteLine($"Your total is: {total}");
+        */
+       ////////////////////////////////////////////////////////////////////////////////////////////////
+
+       // Task 3 - Multiplication Table
        
     }
     
