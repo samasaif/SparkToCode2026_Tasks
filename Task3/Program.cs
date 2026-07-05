4eb8c4d661b9b701bba770fe30931e921ef46b31
sama saif <samaalramadhani@gmail.com>
2026-07-05 17:01:31 +0400

    task 1 - p3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task 1 - Absolute Difference
+        Console.Write("enter the 1st number: ");
+        double firstNumber = double.Parse(Console.ReadLine());
+        
+        Console.Write("enter the 2nd number: ");
+        double secondNumber = double.Parse(Console.ReadLine());
+        
+        double result1 = Math.Abs(secondNumber - firstNumber);
+        
+        Console.WriteLine("absolute difference of " + secondNumber + "-" + firstNumber + " = " + result1);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
     }
 }
