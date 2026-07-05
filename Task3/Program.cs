33ecf29b01b74373fd47aa3199a7c350f6a837be
sama saif <samaalramadhani@gmail.com>
2026-07-05 17:06:50 +0400

    task 2 - p3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Task 1 - Absolute Difference
+        /*
         Console.Write("enter the 1st number: ");
         double firstNumber = double.Parse(Console.ReadLine());
         
@@ -14,6 +15,19 @@ class Program
         double result1 = Math.Abs(secondNumber - firstNumber);
         
         Console.WriteLine("absolute difference of " + secondNumber + "-" + firstNumber + " = " + result1);
+        */
         ///////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        // Task 2 - Power & Root Explorer
+        Console.Write("enter a number: ");
+        double input = double.Parse(Console.ReadLine());
+        
+        double square = Math.Pow(input, 2);
+        double squareRoot = Math.Sqrt(input);
+        
+        Console.WriteLine("square root of " + input + " = " + squareRoot);
+        Console.WriteLine("square of " + input + " = " + square);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
     }
 }
