0fc1b4d35943e4055378e625d617609fe31e5e6e
sama saif <samaalramadhani@gmail.com>
2026-07-07 17:28:40 +0400

    Task 1 - p4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,7 +3,14 @@
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    { 
+        // Task 1 - Personalized Welcome Function
+       Console.Write("enter your name: ");
+       string name = Console.ReadLine();
+       WelcomMssg(name);
     }
+    
+    // Task 1 - Personalized Welcome Function
+    public static void WelcomMssg(string name)
+    { Console.WriteLine($"Welcome {name}!"); }
 }
