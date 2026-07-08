aaa3adc2c71ace76e96d1210c70e894d4b590d8e
sama saif <samaalramadhani@gmail.com>
2026-07-09 00:12:06 +0400

    added try-catch for task 1 - p5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -5,18 +5,32 @@ class Program
     static void Main(string[] args)
     {
         // Task 1 - Fixed Grades Array
+        
         int[] grades = new int[5];
 
         for (int i = 0; i < grades.Length; i++)
         {
-            Console.Write("enter grade " + (i + 1) + " : ");
-            grades[i] = int.Parse(Console.ReadLine());
+            bool valid = false;
+            while (!valid)
+            {
+                try
+                {
+                    Console.Write("enter grade " + (i + 1) + " : ");
+                    grades[i] = int.Parse(Console.ReadLine());
+                    valid = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input... Please enter an integer");
+                }
+            }
         }
         Console.WriteLine("\nGrades:");
         foreach (int grade in grades)
         {
             Console.WriteLine(grade);
         }
+        
         /////////////////////////////////////////////////////////////////////////////////////
     }
 }
