79e8143cbd170faf10586a62b8d9ccaa866f60f8
sama saif <samaalramadhani@gmail.com>
2026-07-09 00:27:05 +0400

    Task 3 - p5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Task 1 - Fixed Grades Array
-        
+        /*
         int[] grades = new int[5];
 
         for (int i = 0; i < grades.Length; i++)
@@ -30,10 +30,11 @@ class Program
         {
             Console.WriteLine(grade);
         }
-        
+        */
         /////////////////////////////////////////////////////////////////////////////////////
         
         // Task 2 - Dynamic To-Do List
+        /*
         List<string> tasks = new List<string>();
         for (int i = 0; i < 5; i++)
         {
@@ -44,6 +45,19 @@ class Program
         foreach (var task in tasks)
         {
             Console.WriteLine(task);
+        }*/
+        /////////////////////////////////////////////////////////////////////////////////////
+        
+        // Task 3 - Browsing History Stack
+        Stack<string> history = new Stack<string>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("enter a website URL " + (i + 1) + " : ");
+            history.Push(Console.ReadLine());
         }
+
+        history.Pop();
+        Console.WriteLine("page: " + history.Peek());
     }
 }
