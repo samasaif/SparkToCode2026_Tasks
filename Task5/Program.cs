e13212403f26770b3c01050af1dbf53793204c1c
sama saif <samaalramadhani@gmail.com>
2026-07-08 23:44:20 +0400

    Task 1 - p5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task 1 - Fixed Grades Array
+        int[] grades = new int[5];
+
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.Write("enter grade " + (i + 1) + " : ");
+            grades[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("\nGrades:");
+        foreach (int grade in grades)
+        {
+            Console.WriteLine(grade);
+        }
+        /////////////////////////////////////////////////////////////////////////////////////
     }
 }
