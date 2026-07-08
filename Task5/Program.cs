fb1d0cfd5c77b67aaec954286dc0b9530bb2d99a
sama saif <samaalramadhani@gmail.com>
2026-07-09 00:53:23 +0400

    task 5 - p5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -64,6 +64,7 @@ class Program
         /////////////////////////////////////////////////////////////////////////////////////
         
         // Task 4 - Customer Service Queue
+        /*
         Queue<string> customers = new Queue<string>();
         for (int i = 0; i < 3; i++)
         {
@@ -72,7 +73,57 @@ class Program
         }
         
         Console.WriteLine("serving: " + customers.Dequeue());
+        */
         /////////////////////////////////////////////////////////////////////////////////////
+        
+        // Task 5 - Array Grade Range
+        int[] grades1 = new int[5];
+        double sum = 0;
+        int highestGrade = 0;
+        int lowestGrade = 0;
+
+        for (int i = 0; i < grades1.Length; i++)
+        {
+            bool valid = false;
+            while (!valid)
+            {
+                try
+                {
+                    Console.Write("enter grade " + (i + 1) + " : ");
+                    grades1[i] = int.Parse(Console.ReadLine());
+                    valid = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input... Please enter an integer");
+                }
+            }
+        }
+        // initialize the highest and lowest value with the first grade in the array 
+        highestGrade = grades1[0];
+        lowestGrade = grades1[0];
+        
+        for (int i = 0; i < grades1.Length; i++)
+        {
+            sum += grades1[i];
+            
+            grades1.Sort();
+            if (grades1[i] > highestGrade)
+            {
+                highestGrade = grades1[i];
+            }
+
+            if (grades1[i] < lowestGrade)
+            {
+                lowestGrade = grades1[i];
+            }
+        }
+        double averageGrade = sum / grades1.Length;
+        Console.WriteLine("\nThe average of the grades is: " + averageGrade);
+        Console.WriteLine("The highest grades is: " + highestGrade);
+        Console.WriteLine("The lowest grades is: " + lowestGrade);
+        /////////////////////////////////////////////////////////////////////////////////////
+
         
         
     }
