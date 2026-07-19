e62b8bcc0022668f16b9636d2e11f63199f28d90
sama saif <samaalramadhani@gmail.com>
2026-07-19 18:40:00 +0400

    created class Room

--- OOP Part 2/Program.cs
+++ OOP Part 2/Program.cs
@@ -1,9 +1,39 @@
 ﻿namespace OOP_Part_2;
+public class Room
+{
+    public int roomNumber;
+    public string roomType;
+    public double pricePerNight;
+    public bool isAvailable;
 
-class Program
+    public Room(int roomNumber, string roomType, double pricePerNight, bool isAvailable)
+    {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.pricePerNight = pricePerNight;
+        this.isAvailable = isAvailable;
+    }
+
+    public void displayRoom()
+    {
+        Console.WriteLine("Room Number: " + roomNumber);
+        Console.WriteLine("Room Type: " + roomType);
+        Console.WriteLine("Room Price per Night: " + pricePerNight);
+
+        if (isAvailable)
+        {
+            Console.WriteLine("Room Available");
+        }
+        else
+        {
+            Console.WriteLine("Room Not Available");
+        }
+    }
+}
+
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
     }
 }
